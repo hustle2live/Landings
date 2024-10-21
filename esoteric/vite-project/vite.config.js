cb6485136020deaf131cab795b40bbd2fede5fe5
hustle2live <kuz.v@ukr.net>
2024-10-21 21:06:55 +0300

    added more two templates

---
+++ esoteric/vite-project/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+
+// css: {
+//    preprocessorOptions: {
+//       scss: {
+//          additionalData: `@import "./src/styles/_variables.scss";`
+//       }
+//    }
+// }
+
+export default defineConfig({
+   base: './'
+});
