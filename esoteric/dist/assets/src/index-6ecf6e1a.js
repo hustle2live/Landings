462e2314c06f6d3cd5c89b0013059cafdafd34bc
hustle2live <kuz.v@ukr.net>
2024-10-28 15:03:43 +0200

    feat: upload full mobile mockup

--- esoteric/dist/assets/src/index-6ecf6e1a.js
+++ esoteric/dist/assets/src/index-6ecf6e1a.js
@@ -41,7 +41,19 @@
 })();
 const style = "";
 const changeThemeButton = document.querySelector(".toggle_theme-btn");
+const dropElements = document.querySelectorAll(".questions__dropdown");
 const htmlElement = document.querySelector("html");
 changeThemeButton.addEventListener("click", () => {
   htmlElement.classList.toggle("dark");
 });
+const closeDrops = () => {
+  dropElements.forEach((el) => el.classList.remove("active"));
+};
+dropElements.forEach(
+  (el) => el.addEventListener("click", () => {
+    const [...classList] = el.classList;
+    closeDrops();
+    if (!classList.includes("active"))
+      el.classList.add("active");
+  })
+);
