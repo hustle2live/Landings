f7115fd0f33f9686c8ac63e134206d9a8c2dfc9b
hustle2live <kuz.v@ukr.net>
2024-10-28 14:29:04 +0200

    feat: modify footer element

--- esoteric/vite-sass/src/js/main.js
+++ esoteric/vite-sass/src/js/main.js
@@ -1,9 +1,22 @@
 import '../scss/style.scss';
 
 const changeThemeButton = document.querySelector('.toggle_theme-btn');
+const dropElements = document.querySelectorAll('.questions__dropdown');
 
 const htmlElement = document.querySelector('html');
 
 changeThemeButton.addEventListener('click', () => {
    htmlElement.classList.toggle('dark');
 });
+
+const closeDrops = () => {
+   dropElements.forEach((el) => el.classList.remove('active'));
+};
+
+dropElements.forEach((el) =>
+   el.addEventListener('click', () => {
+      const [...classList] = el.classList;
+      closeDrops();
+      if (!classList.includes('active')) el.classList.add('active');
+   })
+);
