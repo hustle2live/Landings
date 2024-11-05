fc121bf132187e926f7b2359f9123cf6d712ed64
hustle2live <kuz.v@ukr.net>
2024-11-05 08:17:34 +0200

    feat: added scroll button functionality

--- esoteric/vite-sass/src/js/main.js
+++ esoteric/vite-sass/src/js/main.js
@@ -4,6 +4,7 @@ const changeThemeButton = document.querySelector('.toggle_theme-btn');
 const dropElements = document.querySelectorAll('.questions__dropdown');
 
 const htmlElement = document.querySelector('html');
+const topButton = document.querySelector('.top-btn');
 
 const hearedBtn = document.querySelector('.header__button');
 const formCloseBtn = document.querySelector('.close-btn');
@@ -27,4 +28,23 @@ dropElements.forEach((el) =>
 
 [hearedBtn, formCloseBtn].forEach((element) =>
    element.addEventListener('click', () => form.classList.toggle('hidden'))
+);
+
+let scrollDisabled = false;
+const disableScroll = (disable = true) => (scrollDisabled = disable);
+
+function topButtonShowHandler() {
+   const bodyScrollTop = document.body.scrollTop;
+   const documentScrollTop = document.documentElement.scrollTop;
+
+   if (bodyScrollTop > 1000 || documentScrollTop > 1000) {
+      topButton.style.display = 'block';
+   } else {
+      topButton.style.display = 'none';
+   }
+   disableScroll(false);
+}
+
+window.addEventListener('scroll', () =>
+   scrollDisabled ? null : disableScroll() && setTimeout(topButtonShowHandler, 3000)
 );
