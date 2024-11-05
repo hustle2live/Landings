75231bc56aeffa42be6097dedd2735e77e82c9ec
hustle2live <kuz.v@ukr.net>
2024-11-05 14:49:12 +0200

    feat: added cta hidden functionality

--- esoteric/vite-sass/src/js/main.js
+++ esoteric/vite-sass/src/js/main.js
@@ -5,6 +5,8 @@ const dropElements = document.querySelectorAll('.questions__dropdown');
 
 const htmlElement = document.querySelector('html');
 const topButton = document.querySelector('.top-btn');
+const ctaSection = document.querySelector('.cta');
+const ctaButton = document.querySelector('.cta__button');
 
 const hearedBtn = document.querySelector('.header__button');
 const formCloseBtn = document.querySelector('.close-btn');
@@ -26,25 +28,39 @@ dropElements.forEach((el) =>
    })
 );
 
-[hearedBtn, formCloseBtn].forEach((element) =>
+[hearedBtn, formCloseBtn, ctaButton].forEach((element) =>
    element.addEventListener('click', () => form.classList.toggle('hidden'))
 );
 
 let scrollDisabled = false;
 const disableScroll = (disable = true) => (scrollDisabled = disable);
 
+let scrollPositionPrev = 0;
+
 function topButtonShowHandler() {
    const bodyScrollTop = document.body.scrollTop;
    const documentScrollTop = document.documentElement.scrollTop;
+   const gettingDown = scrollPositionPrev < documentScrollTop;
+
+   if ((bodyScrollTop > 600 || documentScrollTop > 600) && gettingDown) {
+      ctaSection.classList.remove('hidden');
+   } else {
+      ctaSection.classList.add('hidden');
+   }
 
    if (bodyScrollTop > 1000 || documentScrollTop > 1000) {
       topButton.style.display = 'block';
    } else {
       topButton.style.display = 'none';
    }
+
+   scrollPositionPrev = documentScrollTop;
    disableScroll(false);
 }
 
-window.addEventListener('scroll', () =>
-   scrollDisabled ? null : disableScroll() && setTimeout(topButtonShowHandler, 3000)
-);
+window.addEventListener('scroll', () => {
+   if (!scrollDisabled) {
+      disableScroll();
+      setTimeout(topButtonShowHandler, 1200);
+   }
+});
